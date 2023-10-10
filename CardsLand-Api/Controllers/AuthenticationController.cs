31a943383a406717effe46a9d8c9359c73788caf
Esteban Sanchez <easanrui@gmail.com>
2023-10-10 02:10:10 -0600

    First Commit
    Creating api proyect

---
+++ CardsLand-Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardsLand_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+    }
+}
