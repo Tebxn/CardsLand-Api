e90bccd34803af8a8bf521377186b6d9d6b112a2
Esteban Sanchez <easanrui@gmail.com>
2023-11-09 13:48:35 -0600

    Create Entities
    Update UserEnt, Create CardEnt, Create DeckEnt

---
+++ CardsLand-Api/Entities/DeckEnt.cs
@@ -0,0 +1,10 @@
+﻿namespace CardsLand_Api.Entities
+{
+    public class DeckEnt
+    {
+        public long DeckId { get; set; }
+        public string DeckName { get; set; } = string.Empty;
+        public string DeckDescription { get; set; } = string.Empty;
+        public string DeckBackgroundImage { get; set; } = string.Empty;
+    }
+}
