eab5130a990c43d328b97f8b10014a87e959b4da
Esteban Sanchez <easanrui@gmail.com>
2023-11-23 15:07:26 -0600

    Deck controller added

---
+++ CardsLand-Api/Controllers/DeckController.cs
@@ -0,0 +1,51 @@
+﻿using CardsLand_Api.Entities;
+using CardsLand_Api.Interfaces;
+using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CardsLand_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeckController : ControllerBase
+    {
+        private readonly IDbConnectionProvider _connectionProvider;
+        private readonly ITools _tools;
+
+        public DeckController(IDbConnectionProvider connectionProvider, ITools tools)
+        {
+            _connectionProvider = connectionProvider;
+            _tools = tools;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("GetAllDecks/{User_Id}")] //User Decks
+        public async Task<IActionResult> GetAllDecks()
+        {
+            ApiResponse<List<DeckEnt>> response = new ApiResponse<List<DeckEnt>>();
+
+            try
+            {
+                using (var context = _connectionProvider.GetConnection())
+                {
+                    var decks = await context.QueryAsync<DeckEnt>("GetAllDecks", commandType: CommandType.StoredProcedure);
+
+                    response.Success = true;
+                    response.Data = decks.ToList();
+                    return Ok(response);
+                }
+            }
+            catch (SqlException ex)
+            {
+                response.ErrorMessage = "Unexpected Error: " + ex.Message;
+                response.Code = 500;
+                return BadRequest(response);
+            }
+        }
+    }
+}
--- CardsLand-Api/Entities/DeckEnt.cs
+++ CardsLand-Api/Entities/DeckEnt.cs
@@ -3,6 +3,7 @@
     public class DeckEnt
     {
         public long DeckId { get; set; }
+        public long DeckUserId { get; set; } //fk to users
         public string DeckName { get; set; } = string.Empty;
         public string DeckDescription { get; set; } = string.Empty;
         public string DeckBackgroundImage { get; set; } = string.Empty;
