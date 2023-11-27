7a2ee9ae7eb2223597fdbebf5a3f25519cba85c8
Esteban Sanchez <easanrui@gmail.com>
2023-11-27 00:11:26 -0600

    Show user decks

--- CardsLand-Api/Controllers/DeckController.cs
+++ CardsLand-Api/Controllers/DeckController.cs
@@ -24,8 +24,8 @@ namespace CardsLand_Api.Controllers
 
         [HttpGet]
         [Authorize]
-        [Route("GetAllDecks/{User_Id}")] //User Decks
-        public async Task<IActionResult> GetAllDecks()
+        [Route("GetAllUserDecks/{userId}")] //User Decks
+        public async Task<IActionResult> GetAllUserDecks(string userId)
         {
             ApiResponse<List<DeckEnt>> response = new ApiResponse<List<DeckEnt>>();
 
@@ -33,12 +33,101 @@ namespace CardsLand_Api.Controllers
             {
                 using (var context = _connectionProvider.GetConnection())
                 {
-                    var decks = await context.QueryAsync<DeckEnt>("GetAllDecks", commandType: CommandType.StoredProcedure);
+
+                    var data = await context.QueryAsync<DeckEnt>("GetAllUserDecks",
+                    new { User_Id = userId }, commandType: CommandType.StoredProcedure);
 
                     response.Success = true;
-                    response.Data = decks.ToList();
+                    response.Data = data.ToList();
                     return Ok(response);
                 }
+
+            }
+            catch (SqlException ex)
+            {
+                response.ErrorMessage = "Unexpected Error: " + ex.Message;
+                response.Code = 500;
+                return BadRequest(response);
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        [Route("CreateDeck")]
+        public async Task<IActionResult> CreateDeck(DeckEnt entity)
+        {
+            ApiResponse<string> response = new ApiResponse<string>();
+
+            try
+            {
+                using (var context = _connectionProvider.GetConnection())
+                {
+
+                    var data = await context.ExecuteAsync("CreateDeck",
+                    new
+                    {
+                        User_Id = entity.Deck_User_Id,
+                        Deck_Name = entity.Deck_Name,
+                        Deck_Description = entity.Deck_Description,
+                        Deck_BackgroundImage = entity.Deck_Background_Image
+                    },
+                    commandType: CommandType.StoredProcedure);
+
+                    if (data != 0)
+                    {
+                        response.Success = true;
+                        response.Code = 200;
+                        return Ok(response);
+                    }
+                    else
+                    {
+                        response.ErrorMessage = "Email already exists";
+                        response.Code = 500;
+                        return BadRequest(response);
+                    }
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                response.ErrorMessage = "Unexpected Error: " + ex.Message;
+                response.Code = 500;
+                return BadRequest(response);
+            }
+        }
+        [HttpDelete]
+        [Authorize]
+        [Route("DeleteDeck")]
+        public async Task<IActionResult> DeleteDeck(DeckEnt entity)
+        {
+            ApiResponse<string> response = new ApiResponse<string>();
+
+            try
+            {
+                using (var context = _connectionProvider.GetConnection())
+                {
+
+                    var data = await context.ExecuteAsync("DeleteDeck",
+                    new
+                    {
+                        Deck_Id = entity.Deck_Id
+                    },
+                    commandType: CommandType.StoredProcedure);
+
+                    if (data != 0)
+                    {
+                        response.Success = true;
+                        response.Code = 200;
+                        return Ok(response);
+                    }
+                    else
+                    {
+                        response.ErrorMessage = "Server Error";
+                        response.Code = 500;
+                        return BadRequest(response);
+                    }
+                }
+
             }
             catch (SqlException ex)
             {
